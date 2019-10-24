4419494c42e5d20e92f3bd0645a98cfa66a88bec
Chaturanand <chaturre@gmail.com>
2019-10-24 17:48:04 +0545

    Commit Change of Button

---
+++ ChatApplication/src/com/chatur/chat/Client.java
@@ -0,0 +1,96 @@
+package com.chatur.chat;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JTextArea;
+import javax.swing.UIManager;
+
+import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import java.awt.Insets;
+import javax.swing.JTextField;
+
+public class Client extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private String name, address;
+	private int port;
+	private JTextField txtMessage;
+	private JTextArea txtrHistory;
+
+	public Client(String name, String address, int port) {
+		setTitle(" Chat Client");
+		this.name = name;
+		this.address = address;
+		this.port = port;
+		createWindow();
+		console("Attempting a connection to " + address + ":" + port + ", User: " + name);
+		console("Successfuly Disconnected");
+
+	}
+
+	private void createWindow() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// setBounds(100, 100, 459, 529);
+		setSize(800, 550);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[] { 28, 815, 30, 7 }; // sum=880
+		gbl_contentPane.rowHeights = new int[] { 35, 475, 40 }; // sum=550
+		gbl_contentPane.columnWeights = new double[] { 1.0, 1.0 };
+		gbl_contentPane.rowWeights = new double[] { 1.0, Double.MIN_VALUE };
+		contentPane.setLayout(gbl_contentPane);
+
+		txtrHistory = new JTextArea();
+		txtrHistory.setEditable(false);
+		// txtrHistory.setText("history");
+		GridBagConstraints gbc_txtrHistory = new GridBagConstraints();
+		gbc_txtrHistory.insets = new Insets(0, 0, 5, 5);
+		gbc_txtrHistory.fill = GridBagConstraints.BOTH;
+		gbc_txtrHistory.gridx = 1;
+		gbc_txtrHistory.gridy = 1;
+		gbc_txtrHistory.gridwidth = 2;
+		gbc_txtrHistory.insets = new Insets(0, 5, 0, 0);
+		contentPane.add(txtrHistory, gbc_txtrHistory);
+
+		txtMessage = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 0, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 2;
+		contentPane.add(txtMessage, gbc_textField);
+		txtMessage.setColumns(10);
+
+		JButton btnSend = new JButton("Send");
+		GridBagConstraints gbc_btnSend = new GridBagConstraints();
+		gbc_btnSend.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSend.gridx = 2;
+		gbc_btnSend.gridy = 2;
+		contentPane.add(btnSend, gbc_btnSend);
+
+		// requestFocus();
+		txtMessage.requestFocus();
+		setVisible(true);
+		txtMessage.requestFocusInWindow();
+	}
+
+	public void console(String message) {
+		txtrHistory.append(message + "\n\r");
+	}
+
+}
